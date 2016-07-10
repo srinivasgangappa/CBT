f3e05a9772502fc9d77d30155197b68563c00441
srinivasgangappa <srinivasgangappa.87@gmail.com>
2016-07-10 14:16:29 +0530

    test method

---
+++ src/GitHubPackage/TestGitHub.java
@@ -0,0 +1,13 @@
+package GitHubPackage;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TestGitHub {
+
+	@Test
+	public void test()
+	{
+		Reporter.log("Test", true);
+	}
+}
